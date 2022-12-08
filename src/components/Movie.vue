99758eff7fa9a552939f63efaa0adf56f9e68000
huuhkam <huuhkam@dnb.com>
2022-12-08 15:49:00 +0200

    The list of movie

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -2,10 +2,11 @@
 <script>
 import { GET_MOVIES } from "../constants/queries";
 export default {
-  name: "Home",
+  name: "Movie",
   data() {
     return {
-      movies: []
+      movies: [],
+      loading: 0
     };
   },
   apollo: {
@@ -17,38 +18,36 @@ export default {
 </script>
 
 <template>
-  <div class="container">
-    <div class="card">
-      <div class="row mt-1">
-        <table class="table table-bordered">
-          <thead>
-            <tr>
-              <th scope="col">Movie name</th>
-              <th scope="col">Rating</th>
-              <th scope="col">Year</th>
-              <th scope="col">Synopsis</th>
-              <th scope="col">Director</th>
-              <th scope="col">Actors</th>
-              <th scope="col">Genres</th>
-              <th scope="col">AgeLimit</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="item in movies" :key="item.id">
-              <td>{{ item.name }}</td>
-              <td>{{ item.rating }}</td>
-              <td>{{ item.year }}</td>
-              <td>{{ item.synopsis }}</td>
-              <td>{{ item.director.firstName }} {{ item.director.lastName }}</td>
-              <td><span v-for="actor_item in item.actors" :key="actor_item.id">{{ actor_item.firstName }} {{ actor_item.lastName }}, </span></td>
-              <td><span v-for="genre_item in item.genres" :key="genre_item.id">{{ genre_item.genre }}, </span></td>
-              <td>{{ item.ageLimit }}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
+  <h4 v-if="loading">Loading...</h4>
+  <v-row no-gutters justify="start">
+    <v-col v-for="item in movies" :key="item.id">
+      <v-sheet class="pa-1">
+
+        <v-card min-width="300" max-width="400">
+
+          <v-card-item>
+            <v-card-title>{{ item.name }}</v-card-title>
+            <v-card-subtitle>Director: {{ item.director?.firstName }} {{ item.director?.lastName }}</v-card-subtitle>
+            <v-card-subtitle>Rating: {{ item.rating }}</v-card-subtitle>
+            <v-card-subtitle>Age limit: {{ item.ageLimit }}</v-card-subtitle>
+            <v-card-subtitle>Year: {{ item.year }}</v-card-subtitle>
+            <v-card-subtitle>Genre: <span v-for="(genre_item, index) in item.genres" :key="index">{{ genre_item.genre }}<template v-if="index < item.genres.length - 1">, </template> </span></v-card-subtitle>
+          </v-card-item>
+
+          <v-card-text>
+            {{ item.synopsis }}
+          </v-card-text>
+          <v-card-text>
+            <p>Actors:</p>
+            <span v-for="(actor_item, index) in item.actors" :key="index">{{ actor_item.firstName }} {{ actor_item.lastName }}<template v-if="index < item.actors.length - 1">, </template> </span>
+          </v-card-text>
+
+          </v-card>
+
+      </v-sheet>
+    </v-col>
+  </v-row>
+
 </template>
 
 <style scoped>
