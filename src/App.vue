a4700bd4f51dd0345cf691c6bfe7361ad88038a6
huuhkam <huuhkam@dnb.com>
2022-12-09 02:33:04 +0200

    Updated some issues

--- src/App.vue
+++ src/App.vue
@@ -17,24 +17,26 @@ export default {
 <template>
 
   <v-container justify="center">
+    
     <v-app>
-    <v-toolbar app>
-      <v-toolbar-items>
-        <v-btn
-          flat
-          v-for="item in menuItems"
-          :key="item.title"
-          :to="item.path">
-          {{ item.title }}
-        </v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
 
-    <v-divider class="mb-2"></v-divider>
+      <v-toolbar>
+        <v-toolbar-items>
+          <v-btn
+            flat
+            v-for="item in menuItems"
+            :key="item.title"
+            :to="item.path">
+            {{ item.title }}
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
 
-    <v-content>
-      <router-view></router-view>
-    </v-content>
+      <v-divider class="mb-2"></v-divider>
+
+      <v-content>
+        <router-view></router-view>
+      </v-content>
 
     </v-app>
 
