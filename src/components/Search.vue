99d23dc877c7c55884d31d22776cd8faf9993b7b
huuhkam <huuhkam@dnb.com>
2022-12-09 02:34:03 +0200

    Find movie by actor or genre

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,5 +1,5 @@
 <script>
-import { SEARCH_MOVIE, SEARCH_ACTOR } from "../constants/queries";
+import { SEARCH_MOVIE, GET_ACTORS, GET_GENRES, SEARCH_MOVIE_BY_ACTOR, SEARCH_MOVIE_BY_GENRE } from "../constants/queries";
 export default {
   name: "Search",
   data() {
@@ -7,40 +7,80 @@ export default {
       movies: [],
       inputMovie: '',
       searchMovie: '',
-      actors: [],
-      inputActor: '',
+      moviesByActor: [],
+      actorsItem: [],
+      actorsSelected: '',
       searchActor: '',
+      moviesByGenre: [],
+      genresItem: [],
+      genresSelected: '',
+      searchGenre: '',
     };
   },
   apollo: {
     movies: {
-        query() {
-          return SEARCH_MOVIE
-        },
-        variables () {
-          return {
-            term: this.searchMovie
-          }
-        },
-        skip () {
-          return !this.searchMovie
-        },
-        update: data => data.movie        
+      query() {
+        return SEARCH_MOVIE
       },
-      actors: {
-        query() {
-          return SEARCH_ACTOR
-        },
-        variables () {
-          return {
-            term: this.searchActor
-          }
-        },
-        skip () {
-          return !this.searchActor
-        },
-        update: data => data.actor        
+      variables () {
+        return {
+          term: this.searchMovie
+        }
+      },
+      skip () {
+        return !this.searchMovie
+      },
+      update: data => data.movie        
+    },
+    moviesByActor: {
+      query() {
+        return SEARCH_MOVIE_BY_ACTOR
+      },
+      variables () {
+        return {
+          id: this.searchActor
+        }
+      },
+      skip () {
+        return !this.searchActor
+      },
+      update: data => data.moviesByActorId        
+    },
+    actorsItem: {
+      query() {
+        return GET_ACTORS
+      },
+      update: data => {
+          const getActors = []
+          data.actors.map((actor_item, index) => {
+            let newObj = new Object();
+            newObj.name = `${actor_item.firstName} ${actor_item.lastName}`
+            newObj.id = actor_item.id
+            getActors[index] = newObj
+          })
+          return getActors;
       }
+    },
+    moviesByGenre: {
+      query() {
+        return SEARCH_MOVIE_BY_GENRE
+      },
+      variables () {
+        return {
+          id: this.searchGenre
+        }
+      },
+      skip () {
+        return !this.searchGenre
+      },
+      update: data => data.moviesByGenreId        
+    },
+    genresItem: {
+      query() {
+        return GET_GENRES
+      },
+      update: data => data.genres 
+    },
   },
   methods: {
     onEnterMovie (event) {
@@ -49,15 +89,8 @@ export default {
         this.movies = []
         
       this.clearFields("actor")
+      this.clearFields("genre")
       this.searchMovie = this.inputMovie;
-    },
-    onEnterActor (event) {
-      event.preventDefault()
-      if(!this.inputActor)
-        this.actors = []
-
-      this.clearFields("movie")
-      this.searchActor = this.inputActor;
     },
     clearFields(type) {
       switch(type) {
@@ -66,11 +99,31 @@ export default {
           this.movies = []
           break;
         case "actor":
-          this.inputActor = ''
-          this.actors = []
+          this.actorsSelected = ''
+          this.moviesByActor = []
+          break;
+        case "genre":
+          this.genresSelected = ''
+          this.moviesByGenre = []
           break;
       }
     }
+  },
+  watch: {
+    actorsSelected(newSelected, oldSelected) {
+      if(newSelected) {
+        this.clearFields("movie")
+        this.clearFields("genre")
+        this.searchActor = newSelected.id;
+      }
+    },
+    genresSelected(newSelected, oldSelected) {
+      if(newSelected) {
+        this.clearFields("movie")
+        this.clearFields("actor")
+        this.searchGenre = newSelected.id;
+      }
+    },
   }
 };
 </script>
@@ -84,7 +137,7 @@ export default {
 
           <v-text-field
           label="Find movie name"
-          variant="solo"
+          variant="outlined"
           v-model="inputMovie"
           type="input"
           @keydown.enter="onEnterMovie"
@@ -96,14 +149,34 @@ export default {
       <v-col cols="4">
         <v-sheet class="pa-1">
 
-          <v-text-field
-          label="Find actor"
-          variant="solo"
-          v-model="inputActor"
-          type="input"
-          @keydown.enter="onEnterActor"
-        ></v-text-field>
-        
+          <v-autocomplete
+            v-model="actorsSelected"
+            :items="actorsItem"
+            item-title="name"
+            item-value="id"
+            label="Find movie by actor"
+            :menu-props='{ maxHeight: 200}'
+            variant="outlined"
+            return-object
+          ></v-autocomplete>
+
+        </v-sheet>
+      </v-col>
+
+      <v-col cols="4">
+        <v-sheet class="pa-1">
+
+          <v-autocomplete
+            v-model="genresSelected"
+            :items="genresItem"
+            item-title="genre"
+            item-value="id"
+            label="Find movie by genre"
+            :menu-props='{ maxHeight: 200}'
+            variant="outlined"
+            return-object
+          ></v-autocomplete>
+
         </v-sheet>
       </v-col>
       
@@ -117,7 +190,6 @@ export default {
 
               <v-card-item>
                 <v-card-title>{{ movieItem.name }}</v-card-title>
-                <v-card-subtitle>Genre: <span v-for="(genre_item, index) in movieItem.genres" :key="index">{{ genre_item.genre }}<template v-if="index < item.genres.length - 1">, </template> </span></v-card-subtitle>
               </v-card-item>
 
               <v-card-text>
@@ -128,12 +200,37 @@ export default {
           </v-sheet>
         </v-col>
 
-        <v-col v-for="actorItem in actors" :key="actorItem.id">
+        <v-col v-for="movieItem in moviesByActor" :key="movieItem.id">
           <v-sheet class="pa-1">
             <v-card min-width="300" max-width="400">
 
+              <v-card-item>
+                <v-card-title>{{ movieItem.name }}</v-card-title>
+              </v-card-item>
+
               <v-card-text>
-                {{ actorItem.firstName }} {{ actorItem.lastName }}
+                {{ movieItem.synopsis }}
+              </v-card-text>
+              <v-card-text>
+                <p>Actors:</p>
+                <span v-for="(actor_item, index) in movieItem.actors" :key="index">{{ actor_item.firstName }} {{ actor_item.lastName }}<template v-if="index < movieItem.actors.length - 1">, </template> </span>
+              </v-card-text>
+
+            </v-card>
+          </v-sheet>
+        </v-col>
+
+        <v-col v-for="movieItem in moviesByGenre" :key="movieItem.id">
+          <v-sheet class="pa-1">
+            <v-card min-width="300" max-width="400">
+
+              <v-card-item>
+                <v-card-title>{{ movieItem.name }}</v-card-title>
+                <v-card-subtitle>Genre: <span v-for="(genre_item, index) in movieItem.genres" :key="index">{{ genre_item.genre }}<template v-if="index < movieItem.genres.length - 1">, </template> </span></v-card-subtitle>
+              </v-card-item>
+
+              <v-card-text>
+                {{ movieItem.synopsis }}
               </v-card-text>
 
             </v-card>
@@ -143,3 +240,7 @@ export default {
     </v-row>
     
 </template>
+
+<style scoped>
+
+</style>
